21115d793d6456a13e28c0c52d84f8a75b98b6ef
s.santos.pimenta <simoneperiodista3@gmail.com>
2022-01-26 20:25:17 -0300

    Feitas duas tabelas usando anotações e interface Repository para manipular o banco de dados

--- shiftweb5/src/main/java/br/com/fiap/shiftweb5/model/UsuarioModel.java
+++ shiftweb5/src/main/java/br/com/fiap/shiftweb5/model/UsuarioModel.java
@@ -1,18 +1,41 @@
 package br.com.fiap.shiftweb5.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="SHIFT5_USUARIO")
 public class UsuarioModel {
 	
+	@Id
+	@Column(name="ID_USUARIO")
+    @GeneratedValue( strategy = GenerationType.SEQUENCE, generator = "USUARIO_SEQ")
+    @SequenceGenerator( name="USUARIO_SEQ", initialValue = 1, allocationSize = 1)
+	//AllocationSize é especie de um contador que define de quanto em quanto voce quer contar
 	private Long idUsuario;
 	
+	@Column(name="CPF")
     private String cpf;
-    
+	
+	@Column(name="NOME")
     private String nome;
     
+	@Column(name="EMAIL")
     private String email;
     
+	@Column(name="SENHA")
     private String senha;
-    
+	 
+    @Column(name="IDADE")
     private Long idade;
+    
+    @Column(name="FOTO")
+    private String foto;
     
     
 
@@ -30,7 +53,7 @@ public class UsuarioModel {
 
 
 
-	public UsuarioModel(Long idUsuario, String cpf, String nome, String email, String senha, Long idade) {
+	public UsuarioModel(Long idUsuario, String cpf, String nome, String email, String senha, Long idade, String foto) {
 		super();
 		this.idUsuario = idUsuario;
 		this.cpf = cpf;
@@ -38,6 +61,7 @@ public class UsuarioModel {
 		this.email = email;
 		this.senha = senha;
 		this.idade = idade;
+		this.foto = foto;
 	}
 
 	public Long getIdUsuario() {
@@ -87,9 +111,24 @@ public class UsuarioModel {
 	public void setIdade(Long idade) {
 		this.idade = idade;
 	}
+	
+	
+	
 
 
-	//Serve
+	
+	public String getFoto() {
+		return foto;
+	}
+
+
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+
+	//Serve para ver todos os campos com os dados respectivos
 
 	@Override
 	public String toString() {
---
+++ shiftweb5/src/main/java/br/com/fiap/shiftweb5/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package br.com.fiap.shiftweb5.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<> {
+	
+	//O repository tem métodos para fazer o insert, delete, update e buscar dados
+
+}
