c2c74f07dcbe8542294c0f4de9ead191ad5280ce
s.santos.pimenta <simoneperiodista3@gmail.com>
2022-02-02 17:35:49 -0300

    Feitos ajustes vários no controller

--- shiftweb5/src/main/java/br/com/fiap/shiftweb5/controller/CategoriaController.java
+++ shiftweb5/src/main/java/br/com/fiap/shiftweb5/controller/CategoriaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,69 +27,122 @@ public class CategoriaController {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
-	@GetMapping
-	public ResponseEntity<List<CategoriaModel>> findAll() {
-		
-		List<CategoriaModel> lista = categoriaRepository.findAll();
-		return ResponseEntity.ok(lista);
-	}
-	
+	@CrossOrigin
 	@GetMapping("/{id}")
-	public ResponseEntity<CategoriaModel> findiById(@PathVariable("id") Long idCategoria){
-		
-		if(categoriaRepository.existsById(idCategoria)) {
-			CategoriaModel categoriaModel = categoriaRepository.findById(idCategoria).get();
-			return ResponseEntity.ok(categoriaModel);
+	public ResponseEntity<CategoriaModel> findById(@PathVariable("id") Long id){
+		if ( categoriaRepository.existsById(id) ) {
+			CategoriaModel model = categoriaRepository.findById(id).get();
+			
+			return ResponseEntity.ok(model);
 		} else {
 			return ResponseEntity.notFound().build();
 		}
+	}
+	
+//	@CrossOrigin
+//	@GetMapping()
+//	//Este método só vai trazer os elementos que estiverem ativos
+//	public ResponseEntity<List<CategoriaModel>> findAll(){
+//		return ResponseEntity.ok( categoriaRepository.findByAtivo(true) );
+//	}
+	
+	
+//	@CrossOrigin
+//	@GetMapping()
+//	//Este método só vai trazer os elementos do nomeCategoria que contém as letras ne
+//		public ResponseEntity<List<CategoriaModel>> findAll(){
+//			return ResponseEntity.ok( categoriaRepository.findByNomeCategoriaContains("ne") );
+//		}
+	
+//	@CrossOrigin
+//	@GetMapping()
+//	//Este método só vai exibir os nomeCategoria que tem a letra T como inicial
+//		public ResponseEntity<List<CategoriaModel>> findAll(){
+//			return ResponseEntity.ok( categoriaRepository.findByNomeCategoriaLike("T%") );
+//		}
+	
+//	@CrossOrigin
+//	@GetMapping()
+//	//Este método só vai exibir todas as categorias que estão ativas
+//		public ResponseEntity<List<CategoriaModel>> findAll(){
+//		
+//		return ResponseEntity.ok( categoriaRepository.findAll() );
+//		}
+	
+	@CrossOrigin
+	@GetMapping()
+	//Este método só vai exibir todas as categorias que estão ativas
+		public ResponseEntity<Long> findAll(){
 		
-	}
-	
-	@SuppressWarnings("rawtypes")
-	@PostMapping
-	public ResponseEntity post(@RequestBody CategoriaModel categoriaModel) {
-		if(categoriaModel.getNomeCategoria() != null) {
-			categoriaRepository.save(categoriaModel);
-			
-			URI location = ServletUriComponentsBuilder
-					.fromCurrentRequest()
-					.path("/{id}")
-					.buildAndExpand(categoriaModel.getIdCategoria()).toUri();
-			return ResponseEntity.created(location).build();
-			
-		}else {
-			return ResponseEntity.badRequest()
-					.body("O nome da categoria não foi informado");
+		return ResponseEntity.ok( categoriaRepository.getTotalCategorias() );
 		}
-	}
-	
-	//Para o putMapping funcionar tem que ser pasado o ID no body request lá no Postamn
-	
-	@PutMapping("/{id}")
-	public ResponseEntity put(@PathVariable("id") Long idCategoria, @RequestBody CategoriaModel categoriaModel) {
-		if(categoriaRepository.existsById(idCategoria)) {
-			categoriaRepository.save(categoriaModel);
-			categoriaModel.setIdCategoria(idCategoria);
-			return ResponseEntity.ok().build();
-		}else {
-			return ResponseEntity.notFound().build();
-		}
-	}
 	
 	
+		
 	
-	@DeleteMapping("/{id}")
-	public ResponseEntity delete(@PathVariable("id") Long idCategoria) {
-		if(categoriaRepository.existsById(idCategoria)) {
-			categoriaRepository.deleteById(idCategoria);
-			return ResponseEntity.ok().build();
-		} else {
-			return ResponseEntity.noContent().build();
-		}
-	}
-	
-	
+//	@GetMapping
+//	public ResponseEntity<List<CategoriaModel>> findAll() {
+//		
+//		List<CategoriaModel> lista = categoriaRepository.findAll();
+//		return ResponseEntity.ok(lista);
+//	}
+//	
+//	@GetMapping("/{id}")
+//	public ResponseEntity<CategoriaModel> findiById(@PathVariable("id") Long idCategoria){
+//		
+//		if(categoriaRepository.existsById(idCategoria)) {
+//			CategoriaModel categoriaModel = categoriaRepository.findById(idCategoria).get();
+//			return ResponseEntity.ok(categoriaModel);
+//		} else {
+//			return ResponseEntity.notFound().build();
+//		}
+//		
+//	}
+//	
+//	@SuppressWarnings("rawtypes")
+//	@PostMapping
+//	public ResponseEntity post(@RequestBody CategoriaModel categoriaModel) {
+//		if(categoriaModel.getNomeCategoria() != null) {
+//			categoriaRepository.save(categoriaModel);
+//			
+//			URI location = ServletUriComponentsBuilder
+//					.fromCurrentRequest()
+//					.path("/{id}")
+//					.buildAndExpand(categoriaModel.getIdCategoria()).toUri();
+//			return ResponseEntity.created(location).build();
+//			
+//		}else {
+//			return ResponseEntity.badRequest()
+//					.body("O nome da categoria não foi informado");
+//		}
+//	}
+//	
+//	//Para o putMapping funcionar tem que ser pasado o ID no body request lá no Postamn
+//	
+//	@PutMapping("/{id}")
+//	public ResponseEntity put(@PathVariable("id") Long idCategoria, @RequestBody CategoriaModel categoriaModel) {
+//		if(categoriaRepository.existsById(idCategoria)) {
+//			categoriaRepository.save(categoriaModel);
+//			categoriaModel.setIdCategoria(idCategoria);
+//			return ResponseEntity.ok().build();
+//		}else {
+//			return ResponseEntity.notFound().build();
+//		}
+//	}
+//	
+//	
+//	
+//	@DeleteMapping("/{id}")
+//	public ResponseEntity delete(@PathVariable("id") Long idCategoria) {
+//		if(categoriaRepository.existsById(idCategoria)) {
+//			categoriaRepository.deleteById(idCategoria);
+//			return ResponseEntity.ok().build();
+//		} else {
+//			return ResponseEntity.noContent().build();
+//		}
+//	}
+//	
+//	
 
 	
 	
--- shiftweb5/src/main/java/br/com/fiap/shiftweb5/model/CategoriaModel.java
+++ shiftweb5/src/main/java/br/com/fiap/shiftweb5/model/CategoriaModel.java
@@ -1,5 +1,4 @@
 package br.com.fiap.shiftweb5.model;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,44 +9,51 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name="SHIFT5_CATEGORIA")
+
 public class CategoriaModel {
-	
-	@Id
-	@Column(name="ID_CATEGORIA")
+    @Id
+    @Column(name="ID_CATEGORIA")
     @GeneratedValue( strategy = GenerationType.SEQUENCE, generator = "CATEGORIA_SEQ")
-    @SequenceGenerator( name="CATEGORIA_SEQ", initialValue = 1, allocationSize = 1)
-	private Long idCategoria;
-	
-	@Column(name="NOME_CATEGORIA")
-	private String nomeCategoria;
-
-	
-	public CategoriaModel() {
-		super();
-	}
-
-	public CategoriaModel(Long idCategoria, String nomeCategoria) {
-		super();
-		this.idCategoria = idCategoria;
-		this.nomeCategoria = nomeCategoria;
-	}
-
-	public Long getIdCategoria() {
-		return idCategoria;
-	}
-
-	public void setIdCategoria(Long idCategoria) {
-		this.idCategoria = idCategoria;
-	}
-
-	public String getNomeCategoria() {
-		return nomeCategoria;
-	}
-
-	public void setNomeCategoria(String nomeCategoria) {
-		this.nomeCategoria = nomeCategoria;
-	}
-	
-	
-
+    @SequenceGenerator( name = "CATEGORIA_SEQ", initialValue = 1, allocationSize = 1)
+    private Long idCategoria;
+    
+    @Column(name="NOME_CATEGORIA")
+    private String nomeCategoria;
+    
+    @Column(name="ATIVO")   
+    private Boolean ativo;
+    
+    
+    public CategoriaModel() {
+        super();
+    }
+    public CategoriaModel(Long idCategoria, String nomeCategoria) {
+        super();
+        this.idCategoria = idCategoria;
+        this.nomeCategoria = nomeCategoria;
+    }
+    public Long getIdCategoria() {
+        return idCategoria;
+    }
+    public void setIdCategoria(Long idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+    public String getNomeCategoria() {
+        return nomeCategoria;
+    }
+    public void setNomeCategoria(String nomeCategoria) {
+        this.nomeCategoria = nomeCategoria;
+    }
+    public Boolean getAtivo() {
+        return ativo;
+    }
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return "CategoriaModel [idCategoria=" + idCategoria + ", nomeCategoria=" + nomeCategoria + "]";
+    }
 }
